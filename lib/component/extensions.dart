c122225bc4d3225f4f2abacb0c6dea428996a8f8
AfshinNajafi <afshinnajafi74@gmail.com>
2023-06-27 14:18:05 +0300

    refactor(extensions) : refacor date formatter method

--- lib/component/extensions.dart
+++ lib/component/extensions.dart
@@ -5,9 +5,4 @@ extension DateFormatter on String{
     final result = Jalali.now().formatter;
     return "${result.wN} ${result.d} ${result.mN} ${result.yyyy}";
   }
-  /// For the title of the weekly day
-  // weeklyDataCustomFormat() {
-  //   final result = Jalali.now().formatter;
-  //   return "${result.mN}  ${result.yyyy} ";
-  // }
 }
